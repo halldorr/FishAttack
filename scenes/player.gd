763e1b044b130de11a9b1e03ef4f0cea82a2b30f
Jeff Lewis <474547+halldorr@users.noreply.github.com>
2023-07-02 19:30:39 -0300

    Initial commit

---
+++ scenes/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var speed = 300
+
+func _physics_process(_delta):
+	velocity = Vector2(0, 0)
+		
+	if Input.is_action_pressed("Right"):
+		velocity.x = speed
+		
+	if Input.is_action_pressed("Left"):
+		velocity.x = -speed
+	
+	if Input.is_action_pressed("Up"):
+		velocity.y = -speed
+	
+	if Input.is_action_pressed("Down"):
+		velocity.y = speed
+	
+	move_and_slide()
+	
+	var screenSize = get_viewport_rect().size
+	global_position = global_position.clamp(Vector2(0, 0), screenSize)
